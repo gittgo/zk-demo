72942c5284e331c2e3c54387e4e3d9fc3f2e8ca8
Administrator <332860608@qq.com>
2019-04-28 23:06:54 +0800

    Initial commit

---
+++ src/main/java/com/demo/zk/lock/AppTest.java
@@ -0,0 +1,57 @@
+package com.demo.zk.lock;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @ProjectName: zk-demo
+ * @Auther: GERRY
+ * @Date: 2019/01/06 12:07
+ * @Description:
+ */
+public class AppTest {
+    static int n = 10;
+    private static CountDownLatch latch = new CountDownLatch(10);
+
+    public static void secondSkill() {
+        System.out.println(--n);
+
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+
+        TimeUnit.SECONDS.sleep(10);
+        for (int i = 0 ; i <10; i++) {
+            new Thread(){
+                @Override
+                public void run() {
+                    DistributedLock lock = null;
+                    try {
+                        latch.await();
+                    }catch(Exception e){
+                        e.printStackTrace();
+                    }
+
+                    try {
+                        TimeUnit.MILLISECONDS.sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                    try {
+                        lock = new DistributedLock("192.168.100.161:2181,192.168.100.162:2181,192.168.100.163:2181", "test1");
+                        lock.lock();
+                        secondSkill();
+                    } finally {
+                        if (lock != null) {
+                            lock.unlock();
+                        }
+                    }
+
+                }
+            }.start(); //每循环一次，就启动一个线程,具有一定的随机性
+
+            latch.countDown();
+        }
+    }
+}
